71cdf66be84ed568e7a4e1818d0f6423a6f86353
willy <wiha2102@student.miun.se>
2026-06-20 11:07:13 +0200

    fixed the test run script, its small but run some versions supposedly installed

--- test_run.py
+++ test_run.py
@@ -8,7 +8,10 @@ print("GPUs available:", tf.config.list_physical_devices("GPU"))
 
 
 print("Sionna imported successfully")
-print(sn.__)
+print(sn.__version__)
+
+print("Numpy successfully installed")
+print(np.__version__)
 
 # Tiny tensor test (checks TF + GPU execution path)
 x = tf.random.normal([2, 3])
